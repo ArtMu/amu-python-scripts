bf93728c8f38725e24e1449b9d07f16e1f1355d3
Arto Mujunen <artomujunen@gmail.com>
2014-09-09 21:57:39 +0300

    First working new kind of version

--- fileSearch.py
+++ fileSearch.py
@@ -30,74 +30,92 @@ import collections
 
 def main():
 
-	dirOperations()
-
-# Go through the whole file - structure under given directory and save all the founded ".inform" - files to dictionary
-def dirOperations():
+	file_type, path = dir_operations()
+	search_valid_files(file_type, path)
+	
+# Go through the whole file - structure under given directory and save all the founded ".inform" - files 
+# to dictionary
+def dir_operations():
+	""" xxx """
 	out = False
 	path = ""
-
-	# count for give the order number for the file in dictionary
-	count = 0
 	file_extension = '.inform'
-		
-	portsArray = []
+	
 	print "\n################################################################################"
 	print "Special tool for search 'port [32 digit length Hexa value]' - string pair"
 	print "Press < x > to exit" 
+	print "Available Dir(s): "
+	_list_available_dirs()
 	print "################################################################################\n"
 	while (out == False):
-		input = raw_input("Give the input: ")
-		print "You entered ", input
-		if (input == 'x'or input == 'X'):
+		path = raw_input("Give the Directory: ")
+		print "You entered ", path
+		if (path == 'x'or path == 'X'):
 			sys.exit(0)
-		if not os.access(input, os.W_OK):
+		if not os.access(path, os.W_OK):
 			print "Not OK Path"
 			print "Try again"
 		else:
 			print "OK Path"
 			out = True
-
-	# Amu: Choise if need to use input value
 	
-	temp_extension = raw_input("Give the File - type, the extension you want to search. If you give 'x' '.inform' is in use: ")
+	temp_extension = raw_input("""Give the File - type, the extension you want to search.
+	If you give 'x' '.inform' - file type will be used: """)
 	print "You entered: ", temp_extension
 	if not (temp_extension == 'x' or temp_extension == 'X'):
 		file_extension = temp_extension
 	
-	print "File search for %s files will start."
+	print "File search for << %s >> files will start." %(file_extension)
+
+	return file_extension, path
+
+def search_valid_files(file_extension, path):
+	""" This method search all files from the needed file type and save them to list_of_files - dictionary. """
 	# Dictionary for all the inform - files
 	list_of_files = {}
 	
-	# Amu: Used os.walk() is the core of this whole script
-	# Amu: Dictionary for save all .inform files it saves Key as order number and Value as File with path
+	portsArray = []
+	# count for give the order number for the file in dictionary
+	count = 0
+	# Length of file extension used inside next for - loop to check validity of file type (extension length can vary)
+	ex_len = len(file_extension)
+	# Using os.walk() is the core idea of this whole script
+	# Dictionary for save all .inform files it saves Key as order number and Value as File with path
 	for (dirpath, dirnames, filenames) in os.walk(path):
 		for filename in filenames:
-			if filename[-7:] == file_extension: 
+			if filename[-ex_len:] == file_extension: 
 				list_of_files[count] = os.sep.join([dirpath, filename])
 				count = count + 1
 	
 	for value, path in list_of_files.items(): # returns the dictionary as a list of value pairs -- a tuple.
-		# Amu debug
+		# Debug
 		# print "value and path: ", value, path
-		portsArray.extend(gothrough(path))
+		portsArray.extend(_search_from_file(path))
 	
-	printInfo(portsArray)
+	_print_info(portsArray)	
 
-# Amu: Method read the ".inform" - files and save all the port Hexadecimal combinations
-# Return the Array (portArray) of finded matches brake if no maches
-def gothrough(file):
+def _list_available_dirs():
+	""" xxx """
+	 # Handy one liner to save current directory's sub - dirs
+	dirs = os.walk('.').next()[1]
+	for dir in dirs:
+		print "Dir :" + dir
+	
+# Method read the ".inform" - file and save all the port Hexadecimal combinations
+# Return the Array (portArray) of find matches and brake if no matches
+def _search_from_file(file):
+	""" Helper method xxx """
 	print "\n################################################################################"
 	print "Lets seek in the folder in file: ", os.path.abspath(file)
 	print "################################################################################\n"
 	
 	portArray = []
 	
-	# Amu: Regular Expression pattern to get strings with only hexa values and digit amount of between 4 - 32.
-	# Amu Fix: last "\b" was lacking in the end of pattern thats why over 32 digit hexa values were falsely matched
+	# Regular Expression pattern to get strings with only Hexa values and digit amount of between 4 - 32.
+	# Fix: last "\b" was lacking in the end of pattern thats why over 32 digit Hexa values were falsely matched
 	hexaPattern  = re.compile(r'\b[0-9a-fA-F]{4,32}\b') 
 	
-	# Amu: Add data to Array if valid string
+	# Add data to Array if valid string
 	with open(file) as f:
 		for line in f.readlines():
 			if ("port" in line):
@@ -108,14 +126,14 @@ def gothrough(file):
 				
 				# Sub-string which contains stuff after the "port" and 1 space
 				subString = line[ portStartIdx + 5 : len(line)]
-				# Amu comment: Only valid case is one space after "port", not sure if it is an issue.
+				# Comment: Only valid case is one space after "port", not sure if it is an issue.
 				line_after_port = subString.split(' ')
 				
 				# We are interested about the first item line_after_port[0] (port's hexa value)
 				# Clean e.g. tabs and newlines form the line_after_port[0] using strip()
 				data = line_after_port[0].strip()
 					
-				# Amu: Check with Regular Expression pattern if string contain something else than Hexadecimal or it is too long
+				# Check with Regular Expression pattern if string contain something else than Hexadecimal or it is too long
 				hex = re.match(hexaPattern, data)
 				if hex:
 					print "This is MATCH, proper lenght and Hexadecimal", data
@@ -123,23 +141,22 @@ def gothrough(file):
 
 				else:
 					print "This is not MATCH, not Hex or too long", data
-
-			# Amu Debug	
+			# Debug	
 			#else:	
 			#	print "\'port\' - string NOT FOUND in file: ", f.name
 
 	return portArray
 
-# 	
-def printInfo(ports):
-	# Amu: Lets use Counter of Collections to show frequency of similar digits 
+def _print_info(ports):
+	""" Method use Counter of Collections to show frequency of similar digits. 
+	str.format is used for tabulating results """
 	frq_counter = collections.Counter(ports)
-	
+
 	print "\n\n################################################################################"
-	print "		RESULTS:"
-	print "PORT:		FRQ:"
+	print "			RESULTS:"
+	print "PORT:						FRQ:"
 	for portid in frq_counter:
-		print '%s : %d' % (portid, frq_counter[portid])
+		sys.stdout.write("{0:<50}{1:<10}\n".format(portid, frq_counter[portid]))
 	print "################################################################################"
 	
 # boiler
