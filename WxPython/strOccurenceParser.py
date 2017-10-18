c1355110e74efd44970c19705d0aa5ee7331cd3c
Mujunen Arto <artomujunen@gmail.com>
2017-10-18 11:50:48 +0200

    Some fixes to the GUI to usage help

--- WxPython/strOccurenceParser.py
+++ WxPython/strOccurenceParser.py
@@ -7,7 +7,7 @@
 # Currently tool search string pairs: [port/addre/mac/etc..] 1 space [Hexadecimal string]
 # Last one's length is specified by user.
 # User can specify also the first string, suffix of the file. Tool will report findings 
-#    as a content of found strings and the number of occurence of this specific string. 
+#    as a content of found strings and the number of occurrence of this specific string.
 
 # With small modifications to the script more functionality could be added in future: 
 # - If user press specific founded string on list tool could list all the
@@ -30,6 +30,7 @@ class StrFrequentCounter(wx.Panel):
         self.quote0 = wx.StaticText(self, label="Tool which search the frequency of string pairs inside given dir - structure", style=3, pos=(50, 5))
         self.quote1 = wx.StaticText(self, label="Founded Strings:        Frequency of the strings:", pos=(5, 30))
 
+        ### TODO Hard coded location coordinates!! FIX: use constants
         # Buttons
         self.SearchButton = wx.Button(self, label="Search", pos=(350, 325))
         self.Bind(wx.EVT_BUTTON, self.start_search, self.SearchButton)
@@ -38,7 +39,7 @@ class StrFrequentCounter(wx.Panel):
         self.Bind(wx.EVT_BUTTON, self.ExitApp, self.QuitButton)
         
         # open Browser button
-        self.browserButton = wx.Button(self, label='Open browser', pos=(350, 200))
+        self.browserButton = wx.Button(self, label='Open browser', pos=(350, 220))
         self.Bind(wx.EVT_BUTTON, self.open_browser, self.browserButton)    
         
         # File browser
@@ -50,7 +51,7 @@ class StrFrequentCounter(wx.Panel):
         
         # Input text-boxes where user can add values
         self.quoteFileType = wx.StaticText(self, label="File extension:", pos=(350, 40))
-        self.textBoxFileType = wx.TextCtrl(self, pos=(350, 55), value = "log", size=(70, 25))
+        self.textBoxFileType = wx.TextCtrl(self, pos=(350, 55), value = "inform", size=(70, 25))
  
         self.quoteFirstStr = wx.StaticText(self, label="First string [port/addr/item..]:", pos=(350, 80))
         self.textBoxFirstStr = wx.TextCtrl(self, pos=(350, 95), value = "port", size=(70, 25))
@@ -60,7 +61,9 @@ class StrFrequentCounter(wx.Panel):
 
         self.quote4 = wx.StaticText(self, label="Max Length of the string:", pos=(350, 160))
         self.textBoxMaxLen = wx.TextCtrl(self, pos=(350, 175), value = "32", size=(70, 25))
-                
+
+        wx.StaticText(self, label="FIRST CHOOSE THE DIRECTORY!!", pos=(350, 200))
+
         # Available directories Combo-box Control
         #self.quote5 = wx.StaticText(self, label="Founded Directories:", pos=(350, 200))
         #self.sampleList = searchEngine.parser().list_available_dirs()
